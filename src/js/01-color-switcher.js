c294b25c1167433be7288802aebcb9c14dda2b4e
Natasha <natkamonte1992@gmail.com>
2022-11-10 22:25:41 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+startButton.addEventListener('click', onStartClick);
+function onStartClick(event) {
+  startChangeBodyColor = setInterval(
+    () => (body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+  startButton.setAttribute('disabled', true);
+};
+
+stopButton.addEventListener('click', onStopClick);
+function onStopClick() {
+  clearInterval(startChangeBodyColor);
+  startButton.removeAttribute('disabled');
+  body.style.backgroundColor = '#fafafa';
+};
