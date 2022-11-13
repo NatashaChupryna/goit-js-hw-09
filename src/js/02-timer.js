ada484aaaaacc4def49ccb5e24a51e908426eff9
Natasha <natkamonte1992@gmail.com>
2022-11-13 23:24:00 +0200

    .

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,8 +9,10 @@ const refs = {
   minutes: document.querySelector('[data-minutes]'),
   seconds: document.querySelector('[data-seconds]'),
 };
+console.dir(refs.day);
+
+refs.startBtn.setAttribute('disabled', true);
 const currentDate = new Date();
-// console.log(currentDate)
 let selectedDate = '';
 
 const options = {
@@ -19,55 +21,63 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-      console.log(selectedDates[0]);
-      selectedDate = selectedDates[0]; 
-      if (selectedDates[0] < currentDate) {
-          window.alert('Please choose a date in the future');
-          refs.startBtn.setAttribute('disabled', true);
-        } else {
+    console.log(selectedDates[0]);
+    selectedDate = selectedDates[0];
+    if (selectedDates[0] < currentDate) {
+      window.alert('Please choose a date in the future');
+    } else {
       refs.startBtn.removeAttribute('disabled');
-      }
+    }
   },
 };
 
-
-
-let datesDifference = selectedDate - currentDate ;
-console.log(datesDifference)
-
 flatpickr(refs.dateInput, options);
 
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-    return { days, hours, minutes, seconds };
-    
-    refs.day.textContent = days;
-    refs.hours.textContent = hours;
-    refs.minutes.textContent = minutes;
-    refs.seconds.textContent = seconds;
+  return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(datesDifference)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
- 
-const timer = {
-    start() {
-        const currentDate = new Date();
+function setTimerComponents({ days, hours, minutes, seconds }) {
+  refs.day.textContent = '${days}';
+  refs.hours.textContent = '${hours}';
+  refs.minutes.textContent = '${minutes}';
+  refs.seconds.textContent = '${seconds}';
+}
 
-        setInterval(() => {
-            
-        },)
-    }
+const timer = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    this.intervalId = setInterval(() => {
+      if (isActive) {
+        return;
+      }
+      this.isActive = true;
+      const currentDate = new Date();
+      let datesDifference = selectedDate - currentDate;
+      const { days, hours, minutes, seconds} = convertMs(datesDifference);
+      setTimerComponents({ days, hours, minutes, seconds });
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+  },
 };
+
+startButton.addEventListener('click', () => {
+  timer.start();
+});
